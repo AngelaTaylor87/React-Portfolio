92366ea8ad9039228b0340bac86b22836e7df4e2
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-19 18:53:24 -0500

    fixed bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ import React, { useState } from 'react';
 export default function Footer() {
     return (
  
-        <div class="container">
-                        <div class="buttons">
+        <div className="container">
+                        <div className="buttons">
                         
-                                <p><a href="https://www.linkedin.com/in/angela-taylor-89464b103/"><i class="fa fa-fw fa-envelope" id="Resume"></i></a></p> 
-                                <p><a class="button is-primary" href="https://stackoverflow.com/users/15790381/angela"><strong>Stack Overflow</strong></a></p> 
-                                <p><a class="button is-primary" href="https://github.com/AngelaTaylor87"><strong>Github</strong></a></p>
+                                <p><a href="https://www.linkedin.com/in/angela-taylor-89464b103/"><i className="fa fa-fw fa-envelope" id="Resume"></i></a></p> 
+                                <p><a className="button is-primary" href="https://stackoverflow.com/users/15790381/angela"><strong>Stack Overflow</strong></a></p> 
+                                <p><a className="button is-primary" href="https://github.com/AngelaTaylor87"><strong>Github</strong></a></p>
                                 </div>
                         </div>
       
