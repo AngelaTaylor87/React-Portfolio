2f5b8d3847a2c6c55b9928e0066b397bbf730d24
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-19 19:02:35 -0500

    project js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -6,10 +6,10 @@ export default function Project() {
         <h1>Projects</h1>
         <div className="tasks">
             
-                    <a href="https://github.com/Zanderson91/mars-marketplace"></a>
-                    <a href="https://angelataylor87.github.io/Brewery-Finder/"></a>
-                    <a href="https://github.com/AngelaTaylor87/Weather-Dashboard"></a>
-                    <a href="https://github.com/AngelaTaylor87/Code-Quiz"></a>
+                    <a href="https://github.com/Zanderson91/mars-marketplace">mars marketplace</a>
+                    <a href="https://angelataylor87.github.io/Brewery-Finder/">brewery Finder</a>
+                    <a href="https://github.com/AngelaTaylor87/Weather-Dashboard">weather dashboard</a>
+                    <a href="https://github.com/AngelaTaylor87/Code-Quiz">code-quiz</a>
                    
                 </div>
                 </div>
